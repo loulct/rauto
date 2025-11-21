1a7180d79183ddadf9f75850f2b34590562e5701
lou025 <wrallday@gmail.com>
2025-11-21 21:47:45 +0100

    add SpeedInsight tag inside body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
